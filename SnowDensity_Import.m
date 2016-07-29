3ed9f9c3e614e20ff08da471bf46107935cfa29a
apulwicki <apulwicki@gmail.com>
2016-07-29 15:42:54 -0700

    Start of density input

--- SnowDensity_Import.m
+++ SnowDensity_Import.m
@@ -1,4 +1,5 @@
 % Snow density import
 
-display('Density!')
+[snowpit_density, snowpit_text, snowpit_raw] = xlsread('summary_densitydata.xlsx','snowpit','A2:D11');
 
+[SWEtube_density, SWEtube_text, SWEtube_raw] = xlsread('summary_densitydata.xlsx','SWEtube','A2:V32');
