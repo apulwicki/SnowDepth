f919cd8c1dc3e148cb74f0239ccbe7a4a7aba4db
apulwicki <apulwicki@gmail.com>
2016-08-12 14:59:17 -0700

    Working on variogram test

--- variogramtest.m
+++ variogramtest.m
@@ -7,24 +7,33 @@ data = [nanmean(z(5).depth(:,1:4),2), z(5).depth(:,6:7)];
 
 %% Calculate distance between points (lag)
 
+%find the variance for each pair of points
 variotest = zeros(size(data,1)*size(data,1),1);
 for i = 1:size(data,1)
     z = data(i,1); x = data(i,2); y= data(i,3);
     for j = 1:size(data,1)
-        variotest(i*j,1) = round(EuclideanDistance(x,y,data(j,2),data(j,3)),-1);
+        variotest(i*j,1) = EuclideanDistance(x,y,data(j,2),data(j,3)); 
         variotest(i*j,2) = (z-data(j,1))^2;
     end
 end
 
+lag = 10;
+maxlag = round(max(variotest(:,1)),-1);
+intervals = [lag:lag:maxlag]';
 
-variodata = zeros(max(variotest(:,1)),2);
-for n = 1:max(variotest(:,1))
-    index = find(variotest(:,1)==n);
-    variodata(n,1:2) = [n, sum(variotest(index,2))/(2*length(index))];
+variodata = zeros(maxlag/lag,2);
+for n = lag:lag:maxlag+lag
+    index = intersect(find(variotest(:,1)>n), find(variotest(:,1)<n+1));
+    variodata(n/lag,1:2) = [n, sum(variotest(index,2))/(2*length(index))];
 end
+n = [0, lag];
+index = intersect(find(variotest(:,1)>n(1,1)), find(variotest(:,1)<n(1,2)));
+variodata = [[n(1,1), sum(variotest(index,2))/(2*length(index))]; variodata];
+
 
 scatter(variodata(:,1),variodata(:,2))
 
+clear x y z n j i index variotest
 %% Variogram function
 
 z = pulldata(SD,'all','G04','all','LH',1,'fat');
