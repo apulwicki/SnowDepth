a81b02dfd6f13b3a469b56b84ef0ae28a329038b
Alex Pulwicki <apulwicki@gmail.com>
2016-11-09 19:54:20 -0800

    Faster MLR

--- MLRcalval.m
+++ MLRcalval.m
@@ -5,27 +5,24 @@ M = struct2table(X);
 
 n = size(M,2);
 c = logical(dec2bin(0:(2^n)-1)=='1');      c = c(2:end,:);
-mlr_best = cell(length(c),1);    rmse_best = zeros(length(c),1); 
+mlr_best = cell(length(c),1);    rmse_best = zeros(length(c),1);    BIC_best = rmse_best;
 
-runs = 100;        
+runs = 1000;        
 [~, cal_ind] = sort(rand(runs,length(y)),2);
 cal_ind = cal_ind(:,1:floor(length(y)*3/4));
 
 for j = 1:length(c)
     X1 = M(:,c(j,:));  
-
     rmse = zeros(runs,1); mlr = cell(runs,1);
 
     for i = 1:runs
         cal_ind_temp = cal_ind(i,:);
-        val_ind = setdiff(1:length(y),cal_ind_temp);
-        swe_obs = table(y(cal_ind_temp,1), 'VariableNames',{'swe'});
-        X2 = [X1(cal_ind_temp,:), swe_obs];
+        val_ind     = setdiff(1:length(y),cal_ind_temp);
         
-        mlr{i,1} = fitlm(X2);
+        mlr{i,1}    = regress(y(cal_ind_temp,1), [ones(length(cal_ind_temp),1), X1{cal_ind_temp,:}])';
         
-        y_regress = predict(mlr{i,1},X1(val_ind,:));
-        rmse(i,1) = sqrt(sum((y(val_ind,1)-y_regress).^2)/numel(y_regress));
+        y_regress   = sum(X1{val_ind,:}.*mlr{i,1}(2:end),2) + mlr{i,1}(1);
+        rmse(i,1)   = sqrt(sum((y(val_ind,1)-y_regress).^2)/numel(y_regress));
 
     end
 
@@ -33,17 +30,23 @@ for j = 1:length(c)
     %hist(rmse)
     %[h, p]=chi2gof(rmse);    display(['j = ',num2str(j),' h = ',num2str(h), ' p = ',num2str(p)])
 
-    mlr_best{j,1}   = mlr{rmse==min(rmse)};  %choose best coefficients  
+    %Do fitlm for best set
+    min_rmse        = rmse==min(rmse);
+    swe_obs         = table(y(cal_ind(min_rmse,:)), 'VariableNames',{'swe'});
+    topo_obs        = M(cal_ind(min_rmse,:),c(j,:)); 
+    mlr_best{j,1}   = fitlm([topo_obs, swe_obs]);
+    
     rmse_best(j,1)  = min(rmse); %lowest rmse value
-    AIC_best(j,1)   = mlr_best{j,1}.ModelCriterion.AIC;
+    BIC_best(j,1)   = mlr_best{j,1}.ModelCriterion.BIC;
 end
 
-AICweight = exp(-(AIC_best-min(AIC_best))/2); AICweight = AICweight/sum(AICweight);
+BICweight = exp(-(BIC_best-min(BIC_best))/2); BICweight = BICweight/sum(BICweight);
 
+coeffs_w{j,1} = cell(length(mlr_best),1);
 for j = 1:length(mlr_best)
     coeffs_w{j,1} = mlr_best{j,1}.Coefficients(:,1); 
     coeffs_w{j,1}.Properties.VariableNames = {['C',num2str(j)]};
-    coeffs_w{j,1}{:,1} = coeffs_w{j,1}{:,1}*AICweight(j,1);
+    coeffs_w{j,1}{:,1} = coeffs_w{j,1}{:,1}*BICweight(j,1);
 end
 
 all = coeffs_w{end,1}.Properties.RowNames;
