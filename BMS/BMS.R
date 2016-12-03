b444c3061f61491f10a42ab6c695c1f3ff48a7e8
apulwicki <apulwicki@gmail.com>
2016-12-02 16:09:37 -0800

    Working on BMS

--- BMS/BMS.R
+++ BMS/BMS.R
@@ -3,7 +3,7 @@
 
 
 ################# Import Data ###############################
-install.packages(...)
+#install.packages(...)
 
 library(BMS)
 library(R.matlab)
@@ -21,8 +21,8 @@ T = readMat('/home/glaciology1/Documents/Data/SnowDepth/BMS/mat2R.mat')
     tangentCurve = T$topoG4[6]
     Sx = T$topoG4[7]
     centreD = T$topoG4[8]
-G4 = data.frame(T$sweG4, aspect, elevation, northness, profileCurve, slope, tangentCurve, Sx, centreD)
-colnames(G4) = params
+  G4 = data.frame(T$sweG4, aspect, elevation, northness, profileCurve, slope, tangentCurve, Sx, centreD)
+  colnames(G4) = params
 #G2 data
     params = attr(T$topoG2, "dimnames")[[1]]
     params = c("swe",params)
@@ -34,8 +34,8 @@ colnames(G4) = params
     tangentCurve = T$topoG2[6]
     Sx = T$topoG2[7]
     centreD = T$topoG2[8]
-G2 = data.frame(T$sweG2, aspect, elevation, northness, profileCurve, slope, tangentCurve, Sx, centreD)
-colnames(G2) = params
+  G2 = data.frame(T$sweG2, aspect, elevation, northness, profileCurve, slope, tangentCurve, Sx, centreD)
+  colnames(G2) = params
 #G13 data
     params = attr(T$topoG13, "dimnames")[[1]]
     params = c("swe",params)
@@ -47,134 +47,165 @@ colnames(G2) = params
     tangentCurve = T$topoG13[6]
     Sx = T$topoG13[7]
     centreD = T$topoG13[8]
-G13 = data.frame(T$sweG13, aspect, elevation, northness, profileCurve, slope, tangentCurve, Sx, centreD)
-colnames(G13) = params
+  G13 = data.frame(T$sweG13, aspect, elevation, northness, profileCurve, slope, tangentCurve, Sx, centreD)
+  colnames(G13) = params
 
-rm(aspect, elevation, northness, profileCurve, slope, tangentCurve, Sx, centreD,params,swe,T)
-  
+rm(aspect, elevation, northness, profileCurve, slope, tangentCurve, Sx, centreD,params,T)
+
 
 ####### G4 BMS ##########
-  
-  ##uniform prior model
-    #bms
-    #mprior is uniform model prior, used UPI
+
+##uniform prior model
+  #mprior is uniform model prior, used UPI
   attG4 = bms(G4, mprior = "uniform", user.int = F)
-      #get coefficients
-    coef(attG4)
-      #standardized coeffs
+    #get coefficients
+    G4C_uni = coef(attG4,order.by.pip = F,include.constant = T)
+    #standardized coeffs
     coef(attG4, std.coefs = T, order.by.pip = F, include.constant = T)
-      #other results -> mean # of regressors (can also be found using sum(coef(att)[,1]))
+    #other results -> mean # of regressors (can also be found using sum(coef(att)[,1]))
     summary(attG4)
-      #top models
+    #top models
     topmodels.bma(attG4)[,1:3]
-      #get coeffs from these top models
+    #get coeffs from these top models
     beta.draws.bma(attG4)[,1:3]
-      #overview ->blue = positive, red negative, white noninclusion
+    #overview ->blue = positive, red negative, white noninclusion
     image(attG4)
     image(attG4, yprop2pip = T)
-      #examining difference between prior and posterior distibutions
+    #examining difference between prior and posterior distibutions
     plotModelsize(attG4) #-> perhas chose different prior?
-  
-  ##binomial prior model
+
+##binomial prior model
   att_fixedG4 = bms(G4, mprior = "fixed", mprior.size = 2, user.int = T)
-  plotModelsize(att_fixed)
-  
-  ##variable prior model (test how important 'complaints' is)
+    plotModelsize(att_fixedG4)
+    G4C_fix = coef(att_fixedG4, order.by.pip = F, include.constant = T)
+
+##variable prior model (test how important 'complaints' is)
   att_pipG4 = bms(G4, mprior = "pip", mprior.size = c(0.01, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5), user.int = F)
-  coef(att_pipG4)
-  plotModelsize(att_pipG4)
-  
-  ##beta-binomial model priors = default BMS
+    G4C_vari = coef(att_pipG4,order.by.pip = F, include.constant = T)
+    plotModelsize(att_pipG4)
+
+##beta-binomial model priors = default BMS
   att_randomG4 = bms(G4, mprior = "random", mprior.size = 3, user.int = F) #equivalent att_random = bms(G4)
-  plotModelsize(att_randomG4)
-  
-  ##MCMC sampling
-  att_mcmc = bms(G4, burn = 50000, iter = 1e+05, g = "BRIC", mprior = "uniform", nmodel = 2000, mcmc = "bd", user.int = F)
-  plotConv(att_mcmc)
-  summary(att_mcmc)
-  coef(att_mcmc)
-  plotModelsize(att_mcmc)
-  
-  ###comparing models
-  plotComp(Uniform = attG4, Fixed= att_fixedG4, PIP = att_pipG4, Random = att_randomG4, MCMC = att_mcmc)
-  
-  
+    plotModelsize(att_randomG4)
+    G4C_rand = coef(att_randomG4, order.by.pip = F, include.constant = T)
+
+##MCMC sampling
+  att_mcmcG4 = bms(G4, burn = 50000, iter = 1e+05, g = "BRIC", mprior = "uniform", nmodel = 2000, mcmc = "bd", user.int = F)
+    plotConv(att_mcmcG4)
+    summary(att_mcmcG4)
+    G4C_mcmc = coef(att_mcmcG4,order.by.pip = F,include.constant = T)
+    plotModelsize(att_mcmcG4)
+
+###comparing models
+  #PIP
+    plotComp(Uniform = attG4, Fixed= att_fixedG4, PIP = att_pipG4, Random = att_randomG4, MCMC = att_mcmc)
+  #Coefs
+    plotComp(Uniform = G4C_uni, Fixed= G4C_fix)
+
+
+###return coeffs as structure
+G4coeffs = data.frame(G4C_uni, G4C_fix, G4C_rand, G4C_vari, G4C_mcmc)
+
+
+
 ####### G2 BMS ##########
-  
-    ##uniform prior model
-    #bms
-    #mprior is uniform model prior, used UPI
-    attG2 = bms(G2, mprior = "uniform", user.int = F)
-      #get coefficients
-      coef(attG2)
-      #standardized coeffs
-      coef(attG2, std.coefs = T, order.by.pip = T, include.constant = T)
-      #other results -> mean # of regressors (can also be found using sum(coef(att)[,1]))
-      summary(attG2)
-      #top models
-      topmodels.bma(attG2)[,1:3]
-      #get coeffs from these top models
-      beta.draws.bma(attG2)[,1:3]
-      #overview ->blue = positive, red negative, white noninclusion
-      image(attG2)
-      image(attG2, yprop2pip = T)
-      #examining difference between prior and posterior distibutions
-      plotModelsize(attG2) #-> perhas chose different prior?
-    
-    ##binomial prior model
-    att_fixedG2 = bms(G2, mprior = "fixed", mprior.size = 4, user.int = T)
+
+##uniform prior model
+  #mprior is uniform model prior, used UPI
+  attG2 = bms(G2, mprior = "uniform", user.int = F)
+    #get coefficients
+    G2C_uni = coef(attG2,order.by.pip = F,include.constant = T)
+    #standardized coeffs
+    coef(attG2, std.coefs = T, order.by.pip = T, include.constant = T)
+    #other results -> mean # of regressors (can also be found using sum(coef(att)[,1]))
+    summary(attG2)
+    #top models
+    topmodels.bma(attG2)[,1:3]
+    #get coeffs from these top models
+    beta.draws.bma(attG2)[,1:3]
+    #overview ->blue = positive, red negative, white noninclusion
+    image(attG2)
+    image(attG2, yprop2pip = T)
+    #examining difference between prior and posterior distibutions
+    plotModelsize(attG2) #-> perhas chose different prior?
+
+##binomial prior model
+  att_fixedG2 = bms(G2, mprior = "fixed", mprior.size = 4, user.int = T)
     plotModelsize(att_fixed)
-    
-    ##variable prior model (test how important 'complaints' is)
-    att_pipG2 = bms(G2, mprior = "pip", mprior.size = c(0.01, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5), user.int = F)
-    coef(att_pipG2)
+    G2C_fix = coef(att_fixedG2, order.by.pip = F, include.constant = T)
+
+##variable prior model (test how important 'complaints' is)
+  att_pipG2 = bms(G2, mprior = "pip", mprior.size = c(0.01, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5), user.int = F)
+    G2C_vari = coef(att_pipG2,order.by.pip = F, include.constant = T)
     plotModelsize(att_pipG2)
-    
-    ##beta-binomial model priors = default BMS
-    att_randomG2 = bms(G2, mprior = "random", mprior.size = 3, user.int = F) #equivalent att_random = bms(G2)
+
+##beta-binomial model priors = default BMS
+  att_randomG2 = bms(G2, mprior = "random", mprior.size = 3, user.int = F) #equivalent att_random = bms(G2)
     plotModelsize(att_randomG2)
-    
-  ###comparing models
-  plotComp(Uniform = attG2, Fixed= att_fixedG2, PIP = att_pipG2, Random = att_randomG2)  
-  
+    G2C_rand = coef(att_randomG2, order.by.pip = F, include.constant = T)
+
+##MCMC sampling
+  att_mcmcG2 = bms(G2, burn = 50000, iter = 1e+05, g = "BRIC", mprior = "uniform", nmodel = 2000, mcmc = "bd", user.int = F)
+    plotConv(att_mcmcG2)
+    summary(att_mcmcG2)
+    G2C_mcmc = coef(att_mcmcG2,order.by.pip = F,include.constant = T)
+    plotModelsize(att_mcmcG2)
+
+###comparing models
+plotComp(Uniform = attG2, Fixed= att_fixedG2, PIP = att_pipG2, Random = att_randomG2)  
+
+###return coeffs as structure
+G2coeffs = data.frame(G2C_uni, G2C_fix, G2C_rand, G2C_vari, G2C_mcmc)  
+
 ####### G13 BMS ##########
-  
-    ##uniform prior model
-    #bms
-    #mprior is uniform model prior, used UPI
-    attG13 = bms(G13, mprior = "uniform", user.int = F)
-        #get coefficients
-        coef(attG13)
-        #standardized coeffs
-        coef(attG13, std.coefs = T, order.by.pip = T, include.constant = T)
-        #other results -> mean # of regressors (can also be found using sum(coef(att)[,1]))
-        summary(attG13)
-        #top models
-        topmodels.bma(attG13)[,1:3]
-        #get coeffs from these top models
-        beta.draws.bma(attG13)[,1:3]
-        #overview ->blue = positive, red negative, white noninclusion
-        image(attG13)
-        image(attG13, yprop2pip = T)
-        #examining difference between prior and posterior distibutions
-        plotModelsize(attG13) #-> perhas chose different prior?
-  
-    ##binomial prior model
-    att_fixedG13 = bms(G13, mprior = "fixed", mprior.size = 5, user.int = T)
+
+##uniform prior model
+  #mprior is uniform model prior, used UPI
+  attG13 = bms(G13, mprior = "uniform", user.int = F)
+    #get coefficients
+    G13C_uni = coef(attG13,order.by.pip = F,include.constant = T)
+    #standardized coeffs
+    coef(attG13, std.coefs = T, order.by.pip = T, include.constant = T)
+    #other results -> mean # of regressors (can also be found using sum(coef(att)[,1]))
+    summary(attG13)
+    #top models
+    topmodels.bma(attG13)[,1:3]
+    #get coeffs from these top models
+    beta.draws.bma(attG13)[,1:3]
+    #overview ->blue = positive, red negative, white noninclusion
+    image(attG13)
+    image(attG13, yprop2pip = T)
+    #examining difference between prior and posterior distibutions
+    plotModelsize(attG13) #-> perhas chose different prior?
+
+##binomial prior model
+  att_fixedG13 = bms(G13, mprior = "fixed", mprior.size = 5, user.int = T)
     plotModelsize(att_fixed)
-    
-    ##variable prior model (test how important 'complaints' is)
-    att_pipG13 = bms(G13, mprior = "pip", mprior.size = c(0.01, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5), user.int = F)
-    coef(att_pipG13)
+    G13C_fix = coef(att_fixedG13, order.by.pip = F, include.constant = T)
+
+##variable prior model (test how important 'complaints' is)
+  att_pipG13 = bms(G13, mprior = "pip", mprior.size = c(0.01, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5), user.int = F)
+    G13C_vari = coef(att_pipG13,order.by.pip = F, include.constant = T)
     plotModelsize(att_pipG13)
-  
-    ##beta-binomial model priors = default BMS
-    att_randomG13 = bms(G13, mprior = "random", mprior.size = 3, user.int = F) #equivalent att_random = bms(G13)
+
+##beta-binomial model priors = default BMS
+  att_randomG13 = bms(G13, mprior = "random", mprior.size = 3, user.int = F) #equivalent att_random = bms(G13)
     plotModelsize(att_randomG13)
-    
-  ###comparing models
-  plotComp(Uniform = attG13, Fixed= att_fixedG13, PIP = att_pipG13, Random = att_randomG13)    
-  
+    G13C_rand = coef(att_randomG13, order.by.pip = F, include.constant = T)
+
+##MCMC sampling
+  att_mcmcG13 = bms(G13, burn = 50000, iter = 1e+05, g = "BRIC", mprior = "uniform", nmodel = 2000, mcmc = "bd", user.int = F)
+    plotConv(att_mcmcG13)
+    summary(att_mcmcG13)
+    G13C_mcmc = coef(att_mcmcG13,order.by.pip = F,include.constant = T)
+    plotModelsize(att_mcmcG13)
+
+###comparing models
+plotComp(Uniform = attG13, Fixed= att_fixedG13, PIP = att_pipG13, Random = att_randomG13)    
+
+###return coeffs as structure
+G13coeffs = data.frame(G13C_uni, G13C_fix, G13C_rand, G13C_vari, G13C_mcmc)
+
   
   
   
--- BMS/BMSdataGet.m
+++ BMS/BMSdataGet.m
@@ -37,22 +37,71 @@
     
     clear d1* d2* d3*
     
-    %% Get full topo
-    heads = fieldnames(topo_full.G4);
-    for i = 1:3
-        glacier = char(options.glacier(i));
-        for j = 1:length(heads)
-            param = char(heads(j));
-            topoF.(glacier).(param) = topo_full.(glacier).(param)(:);
-        end
-    end    
-    
-    G4topo = topoF.G4; G2topo = topoF.G2; G13topo = topoF.G13; 
-    
-save fullTopo.mat G4topo G2topo  G13topo       
+%% Make table of coeffs
+!R CMD BATCH BMS_matlab.R
+
+load R2mat.mat
+
+tableCol = {'Coefficient','SD', 'PIP','PSP'};
+tableRow = {'aspect','elevation','northness','profileCurv','slope','tangentCurve','Sx','centreD','intercept'};
+
+BMSG4.uniform = table(G4coeffs.Post_Mean,  G4coeffs.Post_SD, G4coeffs.PIP, G4coeffs.Cond_Pos_Sign,...
+                    'RowNames', tableRow, 'VariableNames',tableCol);
+BMSG4.fixed = table(G4coeffs.Post_Mean_1, G4coeffs.Post_SD_1, G4coeffs.PIP_1, G4coeffs.Cond_Pos_Sign_1,...
+                    'RowNames', tableRow, 'VariableNames',tableCol);
+BMSG4.random = table(G4coeffs.Post_Mean_2,G4coeffs.Post_SD_2,  G4coeffs.PIP_2, G4coeffs.Cond_Pos_Sign_2,...
+                    'RowNames', tableRow, 'VariableNames',tableCol);                       
+BMSG4.variable = table(G4coeffs.Post_Mean_3, G4coeffs.Post_SD_3, G4coeffs.PIP_3, G4coeffs.Cond_Pos_Sign_3,...
+                    'RowNames', tableRow, 'VariableNames',tableCol);        
+BMSG4.mcmc = table(G4coeffs.Post_Mean_4, G4coeffs.Post_SD_4, G4coeffs.PIP_4, G4coeffs.Cond_Pos_Sign_4,...
+                    'RowNames', tableRow, 'VariableNames',tableCol);        
         
-        
-        
-        
-        
-        
+BMSG2.uniform = table(G2coeffs.Post_Mean,  G2coeffs.Post_SD, G2coeffs.PIP, G2coeffs.Cond_Pos_Sign,...
+                    'RowNames', tableRow, 'VariableNames',tableCol);
+BMSG2.fixed = table(G2coeffs.Post_Mean_1, G2coeffs.Post_SD_1, G2coeffs.PIP_1, G2coeffs.Cond_Pos_Sign_1,...
+                    'RowNames', tableRow, 'VariableNames',tableCol);
+BMSG2.random = table(G2coeffs.Post_Mean_2,G2coeffs.Post_SD_2,  G2coeffs.PIP_2, G2coeffs.Cond_Pos_Sign_2,...
+                    'RowNames', tableRow, 'VariableNames',tableCol);                       
+BMSG2.variable = table(G2coeffs.Post_Mean_3, G2coeffs.Post_SD_3, G2coeffs.PIP_3, G2coeffs.Cond_Pos_Sign_3,...
+                    'RowNames', tableRow, 'VariableNames',tableCol);        
+BMSG2.mcmc = table(G2coeffs.Post_Mean_4, G2coeffs.Post_SD_4, G2coeffs.PIP_4, G2coeffs.Cond_Pos_Sign_4,...
+                    'RowNames', tableRow, 'VariableNames',tableCol); 
+                
+BMSG13.uniform = table(G13coeffs.Post_Mean,  G13coeffs.Post_SD, G13coeffs.PIP, G13coeffs.Cond_Pos_Sign,...
+                    'RowNames', tableRow, 'VariableNames',tableCol);
+BMSG13.fixed = table(G13coeffs.Post_Mean_1, G13coeffs.Post_SD_1, G13coeffs.PIP_1, G13coeffs.Cond_Pos_Sign_1,...
+                    'RowNames', tableRow, 'VariableNames',tableCol);
+BMSG13.random = table(G13coeffs.Post_Mean_2,G13coeffs.Post_SD_2,  G13coeffs.PIP_2, G13coeffs.Cond_Pos_Sign_2,...
+                    'RowNames', tableRow, 'VariableNames',tableCol);                       
+BMSG13.variable = table(G13coeffs.Post_Mean_3, G13coeffs.Post_SD_3, G13coeffs.PIP_3, G13coeffs.Cond_Pos_Sign_3,...
+                    'RowNames', tableRow, 'VariableNames',tableCol);        
+BMSG13.mcmc = table(G13coeffs.Post_Mean_4, G13coeffs.Post_SD_4, G13coeffs.PIP_4, G13coeffs.Cond_Pos_Sign_4,...
+                    'RowNames', tableRow, 'VariableNames',tableCol);  
+                
+                
+%% Ploting coeffs
+clf
+heads    = fieldnames(BMSG4);
+rowNames = BMSG4.uniform.Properties.RowNames;
+    for j = 1:length(heads)
+        param           = char(heads(j));
+        s1 = subplot(1,3,1); title('G4')
+            plot(1:9,BMSG4.(param){:,1},'o','MarkerSize',10); hold on
+            set(gca,'XTick', 1:length(rowNames), 'XTickLabel',rowNames); rotateticklabel(s1,45);
+        s2 = subplot(1,3,2); title('G2')
+            plot(1:9,BMSG2.(param){:,1},'o','MarkerSize',10); hold on
+            set(gca,'XTick', 1:length(rowNames), 'XTickLabel',rowNames); rotateticklabel(s2,45);            
+        s3 = subplot(1,3,3); title('G13')
+            plot(1:9,BMSG13.(param){:,1},'o','MarkerSize',10); hold on
+            set(gca,'XTick', 1:length(rowNames), 'XTickLabel',rowNames); rotateticklabel(s3,45);
+    end
+          legend(heads,'Location','best')
+
+        fig=gcf; set(findall(fig,'-property','FontSize'),'FontSize',13)
+        fig.PaperUnits = 'inches'; fig.PaperPosition = [0 0 14 7];
+filename = 'BMScoeff_compare';
+print([options.path1, filename],'-dpng','-r0'); print([options.path2, filename],'-dpng','-r0')        
+                
+                
+                
+                
