7ecb2b82bd0e37d3394fc74b27154f3f2d4d5b13
apulwicki <apulwicki@gmail.com>
2017-02-09 16:46:22 -0800

    Kriging work:

--- Kriging/Tutorial - Dice Kriging.R
+++ Kriging/Tutorial - Dice Kriging.R
@@ -1,9 +1,23 @@
+## Load my data ##
+library(R.matlab)
+
+#residuals = readMat('/Users/Alexandra/Documents/SFU/Data/SnowDepth/Kriging/residuals.mat')
+#residuals = readMat('/home/glaciology1/Documents/Data/SnowDepth/Kriging/residuals.mat')
+residuals = readMat('D:/Kriging/residuals.mat')
+res = residuals$res
+utm = residuals$utm
+
+
+############################################################################
+#Dice Kriging
+############################################################################
+
 #install.packages('DiceKriging')
 #install.packages('DiceOptim')
 library(DiceKriging)
 library(DiceOptim)
 
-# 1D Example with known parameters -> simple kriging
+# 1D Example
 inputs = c(-1,-0.5,0,0.5,1)
 output = c(9,-5,1,-9,11)
 theta = 0.4
@@ -22,31 +36,28 @@ plot(t, p$mean, type = "l", xlim = c(-2, 2), ylim = c(-30, 30),
 lines(t, p$lower95, col = "black", lty = 2)
 lines(t, p$upper95, col = "black", lty = 2)
 points(inputs, output, col = "red", pch = 19)
-abline(h = 0)
 
-#Simulations of Gaussian processes underlying Kringing
- #Unconditional
-y = simulate(model, nsim = 5, newdata = data.frame(x=t))
+## My data 1D ##
 
-ytrend = trend[1]+trend[2]*t+trend[3]*t^2
-par(mfrow = c(1, 1))
-plot(t, ytrend, type = "l", col = "black", ylab = "y", lty = "dashed",
-     ylim = c(min(ytrend) - 2 * sigma, max(ytrend) + 2 * sigma))
-for(i in 1:5) lines(t, y[i, ], col = i)
+inputs = utm[,1]
+output = res
+model = km(formula = ~., design = data.frame(x=inputs), nugget.estim = TRUE,
+           response = output, covtype = "matern5_2")
+t = seq(from = 0, to = max(inputs)+100, length = 200)
+p = predict(model, newdata = data.frame(x=t), type = "SK")
 
- #Conditional (known points)
-y = simulate(model, nsim = 5, newdata = data.frame(x=t), cond = TRUE)
-
-plot(t, ytrend, type = "l", col = "black", ylab = "y", lty = "dashed",
-     ylim = c(min(ytrend) - 2 * sigma, max(ytrend) + 2 * sigma))
-for(i in 1:5) lines(t, y[i, ], col = i)
-points(inputs, output, col = "red", pch = 19)
+plot(t, p$mean, type = "l", xlim = c(0, max(t)), ylim = c(0, max(p$mean)+0.1),
+     xlab = "x", ylab = "y")
+  lines(t, p$lower95, col = "black", lty = 2)
+  lines(t, p$upper95, col = "black", lty = 2)
+  points(inputs, output, col = "red", pch = 19)
 
 
-## 2D data
+
+## 2D Example
 X <- expand.grid(x1 = seq(0, 1, length = 4), x2 = seq(0, 1, length = 4))
 y <- apply(X, 1, branin)
-m = km(~., design = X, response = y, covtype = "gauss")
+m = km(~., design = X, response = y, covtype = "gauss",nugget.estim = TRUE)
 
  #concentrates likelihood
 n.grid = 30
@@ -80,19 +91,11 @@ points(X[ , 1], X[ , 2], pch = 19, cex = 1.5, col = "red")
 plot(m)
 
 
-
-## My data
-library(R.matlab)
-
-#residuals = readMat('/Users/Alexandra/Documents/SFU/Data/SnowDepth/Kriging/residuals.mat')
-residuals = readMat('/home/glaciology1/Documents/Data/SnowDepth/Kriging/residuals.mat')
-res = residuals$res
-utm = residuals$utm
-
-X <- data.frame(utm[1:100,])
-y <- res[1:100]
-m = km(~.,design = X, response = y, covtype = "matern5_2")
-m = km(~.^2, design = X, response = y, covtype = "gauss", nugget = 1e-8 * var(y))
+## My data 2D ##
+X <- data.frame(utm)
+y <- res
+m = km(~.,design = X, response = y, covtype = "matern5_2",nugget.estim = TRUE)
+# m = km(~.^2, design = X, response = y, covtype = "gauss", nugget = 1e-8 * var(y))
 #plot(m)
 m
 
@@ -118,45 +121,115 @@ pred.m = predict(m,X.grid,"SK")
 #points(X[ , 1], X[ , 2], pch = 19, cex = 1.5, col = "red")
 contour(x.grid, x.grid, matrix(pred.m$mean, n.grid, n.grid), 60,
         main = "Kriging mean")
-points(X[ , 1], X[ , 2], pch = 19, cex = 1.5, col = "red")
+res_range <- range(res)
+points(X[ , 1], X[ , 2], pch = 19, cex = 1.5, 
+       col = rgb((res-res_range[1])/diff(res_range),0,1-(res-res_range[1])/diff(res_range),1))
+# Color above gradients from blue to red as data varies from min to max.
+
 #contour(x.grid, x.grid, matrix(pred.m$sd^2, n.grid, n.grid), 15,
   #      main = "Kriging variance")
 #points(X[ , 1], X[ , 2], pch = 19, cex = 1.5, col = "red")
 
+# Install and load rgl package
+install.packages("rgl")
+library(rgl)
 
-### 1D attempt
-# 1D Example with known parameters -> simple kriging
-inputs = data.frame(utm[ ,1])
-colnames(inputs) = c("V1")
-output = res
-theta <- 0.04
-model = km(formula = ~., design = inputs, 
-           response = output, covtype = "gauss", nugget = 1e-8 * var(output), coef.cov = theta)
-model
+# Plot surface and observations
+plot3d(X[,1],X[,2],res, xlim=c(0,3000),ylim=c(0,3000),zlim=0:1)
+surface3d(x.grid,x.grid, matrix(pred.m$mean,n.grid,n.grid),col="light blue", alpha=0.5)
 
-t = seq(from = 0, to = max(inputs), length = nrow(inputs)+1)
-t.full = data.frame(t)
-colnames(t.full) = c("V1")
-p = predict(model, newdata = t.full, type = "SK")
-
-plot(t, p$mean, type = "l", xlab = "x", ylab = "y", ylim = c(-0.1, max(res)+0.1), xlim = c(1,max(inputs)))
-  lines(t, p$lower95, col = "black", lty = 2)
-  lines(t, p$upper95, col = "black", lty = 2)
-  points(inputs[ ,1], output, col = "red", pch = 19)
-
-#Simulations of Gaussian processes underlying Kringing
-#Conditional (known points)
-y = simulate(model, nsim = 5, newdata = data.frame(x=t), cond = TRUE)
-ytrend = trend[1]+trend[2]*t+trend[3]*t^2
-par(mfrow = c(1, 1))
-
-plot(t, ytrend, type = "l", col = "black", ylab = "y", lty = "dashed",
-     ylim = c(min(ytrend) - 2 * sigma, max(ytrend) + 2 * sigma))
-for(i in 1:5) lines(t, y[i, ], col = i)
-points(inputs, output, col = "red", pch = 19)
-
+# Plot surface and observations with intervals
+plot3d(X[,1],X[,2],res, xlim=c(0,3000),ylim=c(0,3000),zlim=0:1)
+surface3d(x.grid,x.grid, matrix(pred.m$mean,n.grid,n.grid),col="light blue", alpha=0.5)
+surface3d(x.grid,x.grid, matrix(pred.m$upper95,n.grid,n.grid),col="light blue", alpha=0.25)
+surface3d(x.grid,x.grid, matrix(pred.m$lower95,n.grid,n.grid),col="light blue", alpha=0.25)
+rgl.snapshot("filename.png")
 
 ##############################################################################################
 #GP fit
-#install.packages('GPfit')
+##############################################################################################
 library(GPfit)
+
+#1D Example
+x = seq(from = 0, to = 2*pi, length = 10)
+x = (x-min(x))/(max(x)-min(x))
+y = sin(x)+runif(10)
+y = (y-min(y))/(max(y)-min(y))
+plot(x,y)
+GPmodel = GP_fit(x,y)
+print(GPmodel)
+plot(GPmodel)
+xnew = seq(from = pi, to = 2.5*pi, length = 10)
+GPpredict = predict(GPmodel,xnew)
+plot(xnew, GPpredict$Y_hat)
+points(x, y, col = "red", pch = 9)
+
+## My data 1D ##
+x = utm[,1]
+x = (x-min(x))/(max(x)-min(x))
+y = res
+y = (y-min(y))/(max(y)-min(y))
+plot(x,y)
+GPmodel = GP_fit(x,y)
+print(GPmodel)
+plot(GPmodel)
+xnew = seq(from = 0, to = max(x), length = 200)
+GPpredict = predict(GPmodel,xnew)
+plot(xnew, GPpredict$Y_hat)
+points(x, y, col = "red", pch = 10)
+
+
+#2D Example
+X = matrix(c(1:10,1:10), ncol = 2)
+X[,1] = (X[,1]-min(X[,1]))/(max(X[,1])-min(X[,1]))
+X[,2] = (X[,2]-min(X[,2]))/(max(X[,2])-min(X[,2]))
+y = sin(1:10)+runif(10)
+y = (y-min(y))/(max(y)-min(y))
+GPmodel = GP_fit(X,y)
+print(GPmodel)
+plot(GPmodel)
+
+Xnew = matrix(c(1:8, 1:8), ncol = 2)
+Xnew[,1] = (Xnew[,1]-min(Xnew[,1]))/(max(Xnew[,1])-min(Xnew[,1]))
+Xnew[,2] = (Xnew[,2]-min(Xnew[,2]))/(max(Xnew[,2])-min(Xnew[,2]))
+GPpredict = predict(GPmodel,Xnew)
+
+
+
+## My data 2D ##
+X = utm
+X[,1] = (X[,1]-min(X[,1]))/(max(X[,1])-min(X[,1]))
+X[,2] = (X[,2]-min(X[,2]))/(max(X[,2])-min(X[,2]))
+y = res
+y = (y-min(y))/(max(y)-min(y))
+GPmodel = GP_fit(X,y)
+print(GPmodel)
+plot(GPmodel)
+
+xnew = seq(from = 0, to = max(x), length = 100)
+x.grid <- expand.grid(xnew,xnew)
+GPpredict = predict(GPmodel,x.grid)
+plot(GPmodel, response = FALSE, contour = TRUE)
+
+
+plot(xnew, GPpredict$Y_hat)
+points(x, y, col = "red", pch = 9)
+
+
+contour(xnew, xnew, matrix(GPpredict$Y_hat, 100, 100), 60,
+        main = "Kriging mean")
+res_range <- range(res)
+points(X, pch = 19, cex = 1.5, 
+       col = rgb((res-res_range[1])/diff(res_range),0,1-(res-res_range[1])/diff(res_range),1))
+# Color above gradients from blue to red as data varies from min to max.
+
+#contour(x.grid, x.grid, matrix(pred.m$sd^2, n.grid, n.grid), 15,
+#      main = "Kriging variance")
+#points(X[ , 1], X[ , 2], pch = 19, cex = 1.5, col = "red")
+
+# Plot surface and observations
+plot3d(X[,1],X[,2],res, xlim=0:1,ylim=0:1,zlim=0:1)
+surface3d(xnew,xnew, matrix(GPpredict$Y_hat,100,100),col="light blue", alpha=0.5)
+
+
+
